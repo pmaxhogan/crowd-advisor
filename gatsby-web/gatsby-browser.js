2c54cce97b54f0de2ed70118fc71f8cbfc7e7a90
realprogrammer5000 <me@programmer5000.com>
2021-11-13 14:58:08 -0600

    Add things

---
+++ gatsby-web/gatsby-browser.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/functions"
+
+// console.log(firebase.firestore);
--- gatsby-web/gatsby-config.js
+++ gatsby-web/gatsby-config.js
@@ -3,6 +3,19 @@ module.exports = {
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        {
+            resolve: "gatsby-plugin-firebase",
+            options: {
+                credentials: {
+                    apiKey: "AIzaSyD1RZFCHTu6HCEZV34HJZxIyY4Igi7LbVw",
+                    authDomain: "financial-analyzer.firebaseapp.com",
+                    // databaseURL: "<YOUR_FIREBASE_DATABASE_URL>",
+                    projectId: "financial-analyzer",
+                    storageBucket: "financial-analyzer.appspot.com",
+                    messagingSenderId: "1020474248539",
+                    appId: "1:1020474248539:web:8e369bb2b2a50b53f0de7e"
+                }
+            }
+        }
     ]
 }
