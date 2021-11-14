0571d3dba9efdc74a3d382ce82db0955560f1715
realprogrammer5000 <me@programmer5000.com>
2021-11-13 22:43:40 -0600

    Add masonry layout

--- gatsby-web/src/components/news.jsx
+++ gatsby-web/src/components/news.jsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import {Row, Col, Card, Button} from "react-bootstrap";
-import { Tweet } from 'react-twitter-widgets'
+import "../styles/masonry.css"
+import Masonry from 'react-masonry-css'
 
 
 export default function News(props) {
@@ -9,22 +10,23 @@ export default function News(props) {
     <Row>
       <Col>
           <h3>News</h3>
-          <Row>
-          {props.news && props.news.map(newsArticle => newsArticle &&
-              <Col>
-              <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src={newsArticle.image} />
-                  <Card.Body>
-                      <Card.Title>{newsArticle.headline}</Card.Title>
-                      <Card.Text>
-                          {newsArticle.summary}
-                      </Card.Text>
-                      <a href={newsArticle.url}><Button variant="primary">From {newsArticle.source}</Button></a>
-                  </Card.Body>
-              </Card>
-              </Col>
-          )}
-          </Row>
+          <Masonry
+              breakpointCols={3}
+              className="my-masonry-grid"
+              columnClassName="my-masonry-grid_column">
+              {props.news && props.news.map(newsArticle => newsArticle &&
+                  <Card style={{ width: '18rem' }}>
+                      <Card.Img variant="top" src={newsArticle.image} />
+                      <Card.Body>
+                          <Card.Title>{newsArticle.headline}</Card.Title>
+                          <Card.Text>
+                              {newsArticle.summary}
+                          </Card.Text>
+                          <a href={newsArticle.url}><Button variant="primary">From {newsArticle.source}</Button></a>
+                      </Card.Body>
+                  </Card>
+                  )}
+              </Masonry>
       </Col>
     </Row>
 
