722c2d9fdd2e4bcc64adbececf13772faae0e1db
heartgg <a.szumski98@gmail.com>
2021-11-14 01:09:34 -0600

    incremental

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require("firebase-functions");
 const finnhub = require('./routes/finnhub');
 const twitter = require('./routes/twitter');
 
-exports.updateFinnhubData = functions.https.onRequest(finnhub.updateFinnhubData);
+// twitter routes
 exports.updateSentimentTweetData = functions.https.onRequest(twitter.getSentimentAndTweetHistory);
+
+// finnhub routes
+exports.updateFinnhubData = functions.https.onRequest(finnhub.updateFinnhubData);
 exports.searchPhrase = functions.https.onRequest(finnhub.searchPhrase);
