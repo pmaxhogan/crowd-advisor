be048184669032594d7d247549169f11e93c8e45
heartgg <a.szumski98@gmail.com>
2021-11-13 22:12:57 -0600

    fix sentimentTweet data retrieval

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,6 @@
 const functions = require("firebase-functions");
 const finnhub = require('./routes/finnhub');
-const twitter = require('./routes/twitter')
-const { firebaseAdmin } = require('./firebase');
-
-const db = firebaseAdmin.firestore();
+const twitter = require('./routes/twitter');
 
 exports.updateFinnhubData = functions.https.onRequest(finnhub.updateFinnhubData);
 exports.updateSentimentTweetData = functions.https.onRequest(twitter.getSentimentAndTweetHistory);
