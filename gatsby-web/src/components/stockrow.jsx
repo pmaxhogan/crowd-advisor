6217ec9b9f365c4d91dd659a24ccd1b612f401d1
bradwj <2021bradley@gmail.com>
2021-11-13 19:19:56 -0600

    Stock ticker list

---
+++ gatsby-web/src/components/stockrow.jsx
@@ -0,0 +1,48 @@
+import * as React from "react"
+import { Row, Button } from "react-bootstrap"
+import "../styles/stockrow.css"
+
+const percentChangeStyle = {
+  color: "limegreen",
+  float: "right",
+  pointerEvents: "none"
+}
+
+const tickerStyle = {
+  float: "left",
+  pointerEvents: "none"
+}
+
+const stockRowStyle = {
+  marginBottom: "5px",
+  border: "2px solid black"
+}
+
+export default function StockRow(props) {
+
+  function handleClick(e) {
+    if (props.onClickRow) {
+      props.onClickRow(e.target.value);
+    }
+  }
+
+  return (
+    <Row>
+      <Button 
+        onClick={handleClick}
+        style={stockRowStyle} 
+        variant={"outline-dark"} 
+        size="lg"
+        value={props.stock.ticker}
+      >
+        <label style={tickerStyle}>
+          $<b>{props.stock.ticker}</b>
+        </label>
+
+        <label style={percentChangeStyle}>+5%</label>
+        
+      </Button>
+    </Row>
+
+  );
+}
