52722dd2c892a99950ef8c288236d51741b794d4
realprogrammer5000 <me@programmer5000.com>
2021-11-14 03:03:21 -0600

    Fix

--- gatsby-web/src/components/navbar.jsx
+++ gatsby-web/src/components/navbar.jsx
@@ -48,7 +48,7 @@ class NavbarComponent extends React.Component {
             </Form>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
+              {/*<Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
                 <NavDropdown title="Dropdown" id="basic-nav-dropdown">
@@ -58,7 +58,7 @@ class NavbarComponent extends React.Component {
                   <NavDropdown.Divider/>
                   <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                 </NavDropdown>
-              </Nav>
+              </Nav>*/}
             </Navbar.Collapse>
           </Container>
         </Navbar>
